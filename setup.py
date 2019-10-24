47f5ac3419d3bf6d3338de2e983a86121c71ee70
Michael Bailey <38409252+mister-bailey@users.noreply.github.com>
2019-10-23 23:15:56 -0400

    Updating dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 import imp
 import os.path
 
@@ -6,6 +6,8 @@ p = imp.find_module('numpy')[1]
 include_dir = os.path.join(p,'core','include','numpy')
 setup_dir = os.path.dirname(os.path.realpath(__file__))
 
-ext = Extension('randSAT',['randSAT.cpp','RandGen.cpp','DummyOptions.cpp','core/Solver.cc','utils/System.cc'], include_dirs = [include_dir,setup_dir], extra_compile_args=['-std=c++11'])
+ext = Extension('randSAT',['randSAT.cpp','RandGen.cpp','DummyOptions.cpp','core/Solver.cc','utils/System.cc'],
+               include_dirs = [include_dir,setup_dir], extra_compile_args=['-std=c++11'],
+               install_requires=['numpy'])
 
 setup(name='randSAT', py_modules=['PyRandSAT'], ext_modules=[ext])
